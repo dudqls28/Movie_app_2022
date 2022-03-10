7087ccc7789e05e121c3e0fd5d8c789e313e44b9
youngbeen <dudqls28@naver.com>
2022-03-10 18:30:03 +0900

    3.8 완료

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -1,11 +1,13 @@
 import React , { useEffect, useState } from "react";
 import { View, Text } from "react-native";
 import styled from "styled-components/native";
-import { ActivityIndicator , Dimensions  } from "react-native";
+import { ActivityIndicator , Dimensions , RefreshControl } from "react-native";
 import Swiper from "react-native-swiper";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import Slide from "../components/Slide";
-import Poster from "../components/Poster";
+import HMedia from "../components/HMedia";
+import VMedia from "../components/VMedia";
+
 const API_KEY = "09e026020d0680091279a8fc5d901b35";
 
 const Container = styled.ScrollView``;
@@ -30,17 +32,6 @@ const TrendingScroll = styled.ScrollView`
     margin-top:20px;
 `
 
-const Movie = styled.View`
-    margin-right:20px;
-    align-items: center;
-`
-
-const Title = styled.Text`
-    color: white;
-    font-weight: 600;
-    margin-top : 7px;
-    margin-bottom : 5px;
-`;
 
 const Votes = styled.Text`
     color: rgba(255,255,255,0.8);
@@ -50,34 +41,13 @@ const Votes = styled.Text`
 const ListContainer = styled.View`
     margin-bottom : 40px;
 `
-const HMovie = styled.View`
-    padding: 0px 30px;
-    margin-bottom : 30px;
-    flex-direction: row;
-`
-const HColumn = styled.View`
-    margin-left : 15px;
-    width: 80%;
-`
-
-const Overview = styled.Text`
-    color: white;
-    opacity: 0.8;
-    width: 80%;
-`
-
-const Release = styled.Text`
-    color:white;
-    font-size: 12px;
-    margin-vertical:10px;
-`
-
 const ComingSoonTitle = styled(ListTitle)`
-    margin-bottom: 30px;
+    margin-bottom:20px;
 `
 
 
 const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
+    const [refreshing,setRefreshing] = useState(false);
     const [loading, setLoading] = useState(true);
     const [nowPlaying, setNowPlaying] = useState([]);
     const [upcoming, setUpcoming] = useState([]);
@@ -118,12 +88,17 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
     useEffect (() => {
         getData();
     }, []);
+    const onRefresh = async () => {
+        setRefreshing(true);
+        await getData();
+        setRefreshing(false);
+    }
     return loading ? (
         <Loader>
             <ActivityIndicator />
         </Loader>
     ) : (
-        <Container>
+        <Container refreshControl={<RefreshControl onRefresh={onRefresh} refreshing={refreshing} />}>
             <Swiper
             loop
             horizontal
@@ -148,39 +123,24 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
             <ListTitle>Trending Movies</ListTitle>
             <TrendingScroll contentContainerStyle={{paddingLeft:30}} horizontal showsHorizontalScrollIndicator={false}>
                 {trending.map((movie) => (
-                    <Movie key={movie.id}>
-                        <Poster path={movie.poster_path} />
-                        <Title>
-                            {movie.original_title.slice(0,13)}
-                            {movie.original_title.length > 13 ? "..." : null}
-                        </Title>
-                        <Votes> {movie.vote_average > 0 
-                            ? `⭐️ ${movie.vote_average} /10`
-                            : `comming soon` }</Votes>
-                    </Movie>
+                   <VMedia
+                    key={movie.id}
+                    posterPath={movie.poster_path}
+                    originalTitle={movie.original_title}
+                    voteAverage={movie.vote_average}
+                    />
                 ))}
             </TrendingScroll>
             </ListContainer>
             <ComingSoonTitle>Comming soon</ComingSoonTitle>
             {upcoming.map((movie) =>(
-                <HMovie key ={movie.id}>
-                    <Poster path = {movie.poster_path} />
-                    <HColumn>
-                        <Title>{movie.original_title}</Title>
-                        <Release>
-                            {new Date(movie.release_date).toLocaleDateString("ko",{
-                                month: "long",
-                                day: "numeric",
-                                year: "numeric",
-                            })}
-                        </Release>
-                        <Overview>
-                            {movie.overview !== "" && movie.overview.length > 80
-                                ? `&{movie.overview.slice(0,140)}...`
-                                : movie.overview}
-                        </Overview>
-                    </HColumn>
-                </HMovie>   
+                  <HMedia
+                    key={movie.id}
+                    posterPath={movie.poster_path}
+                    originalTitle={movie.original_title}
+                    overview={movie.overview}
+                    releaseDate={movie.release_date}
+                    />
             ))}
         </Container>
     );
